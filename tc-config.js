34f83717ec000646f2202bcb5160f9df0880bf79
Saidabrorxon06 <saidabrorahmadov@icloud.com>
2025-01-05 05:34:48 +0500

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://MGWNo3MEef2ZXXecG6H3HIGdSO0arMG3_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "AH2-Organizational Environment &amp; Culture"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;The functioning and development of each organization take place in an environment (internal and external). Any action of the organization is possible only if the environment allows it to be carried out. The state and activity of the organization at any moment is the result of the action of its internal factors and the effects of the external environment.&lt;/p&gt;"
+};
